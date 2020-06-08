7cff12be72eb36db5de7532fd302fa28ce33af26
John E Malmberg <john.e.malmberg@intel.com>
2020-06-08 12:10:43 -0500

    CORCI-918 build: Create trusted library
    This is the start of a generic trusted library for Jenkins.
    
    It is intended to contain generic routines that are either common many Jenkins
    projects or use methods that are not allowed in a Jenkins groovy sand
    box.
    
    Signed-off-by: John E Malmberg <john.e.malmberg@intel.com>

---
+++ vars/scmNotifyTrusted.groovy
@@ -0,0 +1,72 @@
+// vars/scmNotifyTrusted.groovy
+
+/**
+ * Simplified scmNotifyTrusted command for GitHub/Gitlab
+ * 
+ * Mimics the notifyGithub API for use with either GitHub or GitLab.
+ *
+ * @param config Map of parameters passed.
+ *
+ * config['status']      Status text.  Required.
+ *                       Should be one of 'PENDING','SUCCESS','FAILURE',
+ *                       or 'ERROR'.
+ *                       For GitLab, this will be translated to 'pending',
+ *                       'success', or 'failed'.
+ * config[credentialsId] Jenkins credential ID that should be used for
+ *                       notifying the SCM repository.
+ *                       Not used for GitLab.
+ * config['description'] Description text for notification.
+ *                       Human readable text.
+ *                       Default is "${env.STAGE_NAME}"
+ *                       Do not use a default for a Matrix Stage.
+ *                       Not used for GitLab.
+ * config['context']     Context for the notification.
+ *                       This is used by GitHub/GitLab to track status
+ *                       changes.
+ *                       Default is "build/${env.STAGE_NAME}".
+ *                       Do not use a default for a Matrix Stage.
+ * config['targetUrl']   Target URL for notification if present.
+ *                       Not used for GitLab.
+ */
+def call(Map config = [:]) {
+
+  if (!config['status']) {
+    error 'scmNotifySystem needs a status parameter value!'
+  }
+
+  def is_github = false
+  def is_gitlab = false
+  if (env.GIT_URL) {
+    // First thing is to determine if this is GitHub or GitLab
+    is_github = env.GIT_URL.contains('.github.com/')
+    is_gitlab = env.GIT_URL.contains('gitlab')
+  }
+  Map params = [:]
+
+  if (is_github) {
+    if (config['credentialsId']) {
+      param['credentialsId'] = config['credentialsId']
+    } else {
+      if (env.SCM_COMMIT_STATUS_ID) {
+        param['credentialsId'] = env.SCM_COMMIT_STATUS_ID
+      } else {
+        steps.println ('Jenkins not configured to notify GitLab')
+      }
+    }
+    params['description'] = config.get('description', env.STAGE_NAME)
+    params['context'] = config.get('context', "build/" + env.STAGE_NAME)
+    params['status'] = config['status']
+    if (config['targetUrl']) {
+      params['targetUrl'] = config['targetUrl']
+    }
+    steps.githubNotify params
+  }
+  if (is_gitlab) {
+    params['state'] = config['status'].toLowerCase()
+    if ((params['state'] == 'failure') || (params['state'] == 'error')) {
+        params['state'] = 'failed'
+    }
+    params['name'] = config.get('context', "build/" + env.STAGE_NAME)
+    steps.updateGitlabCommitStatus params
+  }
+}
