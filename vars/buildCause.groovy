9c1256dac76ee5a58b5806af817d6ca17cee6d65
Brian J. Murrell <brian.murrell@intel.com>
2020-12-09 19:47:13 -0500

    Updates

--- vars/buildCause.groovy
+++ vars/buildCause.groovy
@@ -7,7 +7,13 @@ Boolean call(Map config = [:]) {
 
   String buildUser = "Unknown"
   println "CAUSE ${currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause).properties}"
-  //[userName:Brian J. Murrell, userIdOrUnknown:bmurrell, userId:bmurrell, class:class hudson.model.Cause$UserIdCause, userUrl:user/bmurrell, shortDescription:Started by user Brian J. Murrell]
+  /* [userName:Brian J. Murrell,
+      userIdOrUnknown:bmurrell,
+      userId:bmurrell,
+      class:class hudson.model.Cause$UserIdCause,
+      userUrl:user/bmurrell,
+      shortDescription:Started by user Brian J. Murrell]
+  */
   def causes = currentBuild.getBuildCauses()
 
   // Get a specific Cause type (in this case the user who kicked off the build),
@@ -16,6 +22,7 @@ Boolean call(Map config = [:]) {
   println "specificCause: " + specificCause
 
   println currentBuild.getBuildCauses().shortDescription[0]
+
 
   /*
   def buildCauses = currentBuild.rawBuild.getCauses()
@@ -34,5 +41,5 @@ Boolean call(Map config = [:]) {
   }*/
   echo "Initiated by: ${buildUser}"
 
-  return buildUser == "TimerTrigger"
+  return !currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').isEmpty()
 }
