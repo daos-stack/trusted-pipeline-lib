d3744dd3ef3005137e2b3d9868bafbd5257b07d1
Brian J. Murrell <brian.murrell@intel.com>
2020-12-09 20:11:10 -0500

    Fix syntax again

--- vars/buildCause.groovy
+++ vars/buildCause.groovy
@@ -6,7 +6,7 @@
 Boolean call(Map config = [:]) {
 
   String buildUser = "Unknown"
-  if (${currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause}) {
+  if (${currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)}) {
     println "CAUSE ${currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause).properties}"
   } else {
     println "currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause) is null"
