42cb40d8105937df68cdc809d21f692cf768f53d
Brian J. Murrell <brian.murrell@intel.com>
2021-01-12 16:19:40 -0500

    Use env.COMMIT_MESSAGE for finding commit pragmas (#7)
    If it exists.  But be backwards compatible with existing behaviour.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/commitPragmaTrusted.groovy
+++ vars/commitPragmaTrusted.groovy
@@ -15,15 +15,34 @@ def call(Map config = [:]) {
     if (config['def_val']) {
         def_value = config['def_val']
     }
-    def value = sh(script: '''b=$(git show -s --format=%B |
-                                  sed -ne 's/^''' + config['pragma'] +
-                           ''': *\\(.*\\)/\\1/p')
-                              if [ -n "$b" ]; then
-                                  echo "$b"
-                              else
-                                  echo "''' + def_value + '''"
-                              fi''',
-                returnStdout: true)
-    return value.trim()
+
+    String commit_message=""
+    /* TODO: Replace all of this with currentBuild->changeSets
+     *       https://build.hpdd.intel.com/pipeline-syntax/globals#currentBuild
+     *       Something along the lines of:
+     *       def changeLogSets = currentBuild.changeSets
+     *       for (int i = 0; i < changeLogSets.size(); i++) {
+     *           def entries = changeLogSets[i].items
+     *           for (int j = 0; j < entries.length; j++) {
+     *               def entry = entries[j]
+     *               commit_message= $entry.msg
+     *           }
+     *       }
+     */
+    if (env.COMMIT_MESSAGE && env.COMMIT_MESSAGE != "") {
+        commit_message = env.COMMIT_MESSAGE
+    } else {
+        commit_message = sh(script: 'git show -s --format=%B',
+                            returnStdout: true).trim()
+    }
+    return sh(script: 'b=$(echo "' + commit_message + '''" |
+                           sed -ne 's/^''' + config['pragma'] +
+                    ''': *\\(.*\\)/\\1/p')
+                       if [ -n "$b" ]; then
+                           echo "$b"
+                       else
+                           echo "''' + def_value + '''"
+                       fi''',
+              returnStdout: true).trim()
 
 }
