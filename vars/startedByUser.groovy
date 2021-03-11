390e0722ec6609bbf3d3a0ca32c1c6ae2d049589
Brian J. Murrell <brian.murrell@intel.com>
2021-03-11 17:30:47 -0500

    Add a startedByUser() test (#9)

--- vars/startedByUser.groovy
+++ vars/startedByUser.groovy
@@ -1,13 +1,13 @@
 // vars/startedByTimer.groovy
 
 /**
- * Return True the build was started by a timer
+ * Return True if the build was caused by a User (I.e. Build Now)
  * 
  * This needs to be here rather than in pipeline-lib or Jenkinsfile due to
  * Scripts not permitted to use method net.sf.json.JSON isEmpty.
  */
 Boolean call(Map config = [:]) {
 
-  return !currentBuild.getBuildCauses('hudson.triggers.TimerTrigger$TimerTriggerCause').isEmpty()
+  return !currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').isEmpty()
 
 }
