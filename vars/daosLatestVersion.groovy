a63cde7c86833920988ccb8b46af9113ca7776f3
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-09-18 23:08:55 +0200

    Fix: add missing function argument (9th)
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- vars/daosLatestVersion.groovy
+++ vars/daosLatestVersion.groovy
@@ -36,8 +36,7 @@ String getLatestVersion(String distro, BigDecimal next_version, String type='sta
                script: '$(command -v dnf) --refresh repoquery --repofrompath=daos,' + env.ARTIFACTORY_URL +
                        '/artifactory/' + repo +
                      ''' --repoid daos --qf %{version}-%{release} --whatprovides 'daos < ''' +
-                                  next_version + '''' |
-                              rpmdev-sort | tail -1''',
+                                  next_version + '''' 2>/dev/null | rpmdev-sort | tail -1''',
                returnStdout: true).trim()
     /* groovylint-disable-next-line CatchException */
     } catch (Exception e) {
